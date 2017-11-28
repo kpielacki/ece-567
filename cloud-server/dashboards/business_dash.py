04b162ca16e95501bbcd1bd09303b9c78858ce81
Kevin <kpielacki@gmail.com>
2017-11-28 13:17:34 -0500

    Business dashboard profile and activity

--- cloud-server/dashboards/business_dash.py
+++ cloud-server/dashboards/business_dash.py
@@ -1,7 +1,6 @@
 from plotly_app import app
 from admin_app_config import db
 
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -28,38 +27,112 @@ with app.server.app_context():
                       for row in query.all()]
 
     query = db.session.query(
-        User.email.distinct().label('s'))
-    email_options = [{'label': row.s, 'value': row.s}
+        User.id.distinct().label('s'))
+    uid_options = [{'label': str(row.s), 'value': str(row.s)}
                      for row in query.all()]
     
     layout = html.Div([
         html.Div([
             html.H1('Business Portal'),
             dcc.Dropdown(
-                id='email-dropdown',
-                options=email_options,
-                value=email_options[1]['value']
+                id='uid-dropdown',
+                options=uid_options,
+                value='1',
             ),
             html.Div([
                 html.H3('Risk Overview'),
-                html.Div(['TEMP'], id='content'),
+                html.Div(['Loading...'], id='content'),
             ]),
         ], className="container")
     ], style={'padding-bottom': '20px'})
+
+
+    def get_profile(uid):
+        result = db.session.query(User).filter(User.id == uid).first()
+        email = 'N/A'
+        gender = 'N/A'
+        age = 'N/A'
+        if result is not None:
+            email = str(result.email).capitalize()
+            gender = str(result.gender).capitalize()
+            if result.birthday is None:
+                age = 'N/A'
+            else:
+                age = (datetime.date.today() - result.birthday).days / 365
+                if age <= 1:
+                    age = (datetime.date.today() - result.birthday).days
+                    age = str(age) + ' Days'
+                else:
+                    age = str(age) + ' Years'
+        return email, age, gender
+
+    def get_activity(uid):
+        date_from = datetime.date.today() - datetime.timedelta(days=365)
+        result = db.session.query(UserSteps) \
+            .filter(UserSteps.user_id == uid) \
+            .filter(UserSteps.date >= date_from) \
+            .order_by(UserSteps.date.desc()).all()
+        xs = []
+        ys = []
+        for r in result:
+            xs.append(r.date)
+            ys.append(r.step_count)
+
+        {
+            'data': [{
+                'x': xs,
+                'y': ys
+            }],
+            'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+        }
+
+        return dcc.Graph(
+            id='health-trend',
+            figure={
+                'data': [{
+                'x': xs,
+                'y': ys
+                }],
+                'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+            },
+            style={'max-height': '300px'}
+        )
     
     @app.callback(Output("content", "children"),
-                  [Input('email-dropdown', 'value')])
-    def update_info(email):
-        result = db.session.query(User).filter(User.email == email).first()
-        if result is None:
-            gender = 'N/A'
+                  [Input('uid-dropdown', 'value')])
+    def update_info(uid):
+        try:
+            email, age, gender = get_profile(uid)
+        except:
+            email = 'N/A'
             age = 'N/A'
-        else:
-            gender = str(result.gender)
-            age = str(result.birthday)
+            gender = 'N/A'
+
+        try:
+            overview = get_overview(uid)
+        except:
+            overview = html.P('Overview Not Available')
+
+        try:
+            activity_trend = get_activity(uid)
+        except:
+            activity_trend = html.P('Activity Not Available')
 
         return html.Div([
-            html.H4(email),
-            html.H4(gender),
-            html.H4(age),
+            html.Div([
+                html.H4('Profile'),
+                html.Ul([
+                    html.Li('Email: {}'.format(email)),
+                    html.Li('Gender: {}'.format(gender)),
+                    html.Li('Age: {}'.format(age))
+                ]),
+            ]),
+            html.Div([
+                html.H4('Overview'),
+                overview
+            ]),
+            html.Div([
+                html.H4('Year Step Activity'),
+                activity_trend
+            ]),
         ])
