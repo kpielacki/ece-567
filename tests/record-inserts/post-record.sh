993633dcf4eebb6870d608933141afe0fc2b64a2
Kevin <kpielacki@gmail.com>
2017-11-29 13:01:26 -0500

    Fixed bash header and detail on duplicate entry output

--- tests/record-inserts/post-record.sh
+++ tests/record-inserts/post-record.sh
@@ -1,11 +1,14 @@
-!#/bin/bash
+#!/bin/bash
 
 echo ""
 echo "Used to test POST data upload to tables user_location and user_steps"
 echo ""
-echo "Should return JSON:"
+echo "Should return JSON upon first success:"
 echo "  200: Record Insert Successful"
 echo "  200: Record Insert Successful"
+echo "Should return JSON upon after record insert failing unique constraint:"
+echo "  400: Malformed Data Entered"
+echo "  400: Malformed Data Entered"
 
 echo ""
 echo "========= RESULTS =========="
